838c2590491bf80a5b27ea5212c8c0b6ab15782e
udhayakumar0704 <126324521+udhayakumar0704@users.noreply.github.com>
2023-07-18 15:05:25 +0530

    Add files via upload

---
+++ important programs/codissia.java
@@ -0,0 +1,11 @@
+package startup;
+import java.util.Scanner;
+public class company {
+	public static void main(String[] args)
+	{
+	Scanner sc=new Scanner(System.in); 
+	int a=sc.nextInt();
+	System.out.println("Number of Events hosted in coddisia is" +a); 
+
+}
+}
