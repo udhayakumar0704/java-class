8ade4a1d11458fe59f597c07ef0e4848bf792e37
udhayakumar0704 <126324521+udhayakumar0704@users.noreply.github.com>
2023-07-19 12:37:52 +0530

    Add files via upload

---
+++ important programs/x=20 and y=30.java
@@ -0,0 +1,22 @@
+package nested;
+
+import java.util.Scanner;
+
+public class Decision3 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int x,y;
+		x=s.nextInt();
+		y=s.nextInt();
+		if(x==20)
+		{
+			if(y==30)
+			{
+				System.out.println("x=20 and y=30");
+			}
+
+	}
+
+}
+}
