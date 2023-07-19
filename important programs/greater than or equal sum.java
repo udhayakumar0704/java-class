63a1c18de7410171e97013895349c6dbfc3f538e
udhayakumar0704 <126324521+udhayakumar0704@users.noreply.github.com>
2023-07-19 11:19:12 +0530

    Add files via upload

---
+++ important programs/greater than or equal sum.java
@@ -0,0 +1,18 @@
+package decision;
+
+import java.util.Scanner;
+
+public class Decision1 {
+	
+	public static void main(String[] args) {
+		Scanner e = new Scanner(System.in);
+		int x;
+		x=e.nextInt();
+		if(x>=10)
+		{
+			System.out.println("X is greater than or equal to  10");
+		}
+		
+	}
+
+}
